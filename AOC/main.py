0a15a3d96d08065c1548696fc81234c82ad136b9
Jakub Stokowski <jakubstokowski@tutanota.com>
2021-12-03 10:34:43 +0100

    Day2

--- AOC/main.py
+++ AOC/main.py
@@ -1,4 +1,8 @@
-from AOC.Day1 import SonarSweep
+from AOC.Day1 import sonar_sweep
+from AOC.Day2 import dive
 
-SonarSweep.sonar_sweep1()
-SonarSweep.sonar_sweep2()
+sonar_sweep.sonar_sweep1()
+sonar_sweep.sonar_sweep2()
+
+dive.dive1()
+dive.dive2()
