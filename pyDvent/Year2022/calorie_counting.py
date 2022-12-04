161972d3e3701d7ef68f568f2d1ed2da4d1a019e
Jakub Stokowski <jakubstokowski@tutanota.com>
2022-12-04 16:13:23 +0100

    Refactor

--- pyDvent/Year2022/calorie_counting.py
+++ pyDvent/Year2022/calorie_counting.py
@@ -1,18 +1,6 @@
 def calorie_counting(file_path):
-    all_records = []
-    with open(file_path, 'r') as calories:
-        total = 0
-        for entry in calories:
-            if entry == '\n':
-                all_records.append(total)
-                total = 0
-            else:
-                total += int(entry)
+    with open(file_path, 'r') as input_file:
+        calories = [sum(list(map(int, x.split('\n')))) for x in input_file.read().split('\n\n')]
 
-    all_records.sort(reverse=True)
-    result1 = all_records[0]
-    result2 = sum(all_records[0:3:1])
-
-    return result1, result2
-
-
+    calories.sort(reverse=True)
+    return calories[0], sum(calories[0:3])
