1ce2495d5a9dc6efe4b0871595ce62340724d703
Jakub Stokowski <jakubstokowski@tutanota.com>
2022-12-07 15:19:08 +0100

    Refactor

--- pyDvent/Year2022/no_space_left_on_device.py
+++ pyDvent/Year2022/no_space_left_on_device.py
@@ -1,60 +1,45 @@
 class Directory:
-    def __init__(self, name):
+    def __init__(self, name, parent):
         self.name = name
-        self.size = 0
         self.content = dict()
+        self.content['..'] = parent
+
+    def size(self):
+        return sum(v.size() for _, v in list(filter(lambda p: p[0] != '..', self.content.items())))
 
 
 class File:
     def __init__(self, name, size):
         self.name = name
-        self.size = size
+        self.__size = size
+
+    def size(self):
+        return self.__size
 
 
 def no_space_left_on_device(input_path):
     with open(input_path, 'r') as input_file:
-        commands = input_file.readlines()
-        home = Directory('/')
-        home.content['..'] = home
+        home = Directory('/', None)
 
         curr = home
-        for command in commands[1:]:
-            command.replace('\n', '')
-            command_parsed = command.split()
-            if command_parsed[0] == '$':
-                func = command_parsed[1]
-                arg = command_parsed[2] if len(command_parsed) > 2 else ""
-                if func == 'cd':
-                    curr = curr.content[arg]
-                elif func == 'ls':
-                    pass  # ¯\_(ツ)_/¯
-            else:
-                if command_parsed[0] == 'dir':
-                    curr.content[command_parsed[1]] = Directory(command_parsed[1])
-                    curr.content[command_parsed[1]].content['..'] = curr
-                elif command_parsed[0].isnumeric():
-                    curr.content[command_parsed[1]] = File(command_parsed[1], int(command_parsed[0]))
+        for command in input_file.readlines()[1:]:
+            match command.replace('\n', '').split():
+                case ['$', func, arg]:
+                    curr = curr.content[arg] if func == 'cd' else curr
+                case ['dir', name]:
+                    curr.content[name] = Directory(name, curr)
+                case [size, name] if size.isnumeric():
+                    curr.content[name] = File(name, int(size))
 
-        def build_dir_sizes(curr_dir):
-            for k, v in curr_dir.content.items():
-                if type(v) is File:
-                    curr_dir.size += v.size
-                elif type(v) is Directory and k != '..':
-                    curr_dir.size += build_dir_sizes(v)
+        def flatten(curr_dir):
+            ls = [(curr_dir, curr_dir.size())]
+            for v in filter(lambda p: type(p[1]) is Directory and p[0] != '..', curr_dir.content.items()):
+                ls.extend(flatten(v[1]))
+            return ls
+        all_dirs = flatten(home)
 
-            return curr_dir.size
-        build_dir_sizes(home)
-
-        def build_dir_list(curr_dir, dir_list):
-            dir_list.append(curr_dir)
-            for k, v in curr_dir.content.items():
-                if type(v) is Directory and k != '..':
-                    build_dir_list(v, dir_list)
-        all_dirs = []
-        build_dir_list(home, all_dirs)
-
-        result1 = sum(map(lambda d: d.size, filter(lambda d: d.size <= 100000, all_dirs)))
-        required = 30000000 - (70000000 - home.size)
-        result2 = min(filter(lambda d: d.size >= required, all_dirs), key=lambda d: d.size).size
+        result1 = sum(map(lambda d: d[1], filter(lambda d: d[1] <= 100000, all_dirs)))
+        required = 30000000 - (70000000 - home.size())
+        result2 = min(filter(lambda d: d[1] >= required, all_dirs), key=lambda d: d[1])[1]
 
         return result1, result2
